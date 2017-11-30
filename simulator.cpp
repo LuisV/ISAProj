51c2ad01ad8e42ae9e319334ceaa47375e3fa1bd
Judith Ramirez <judramirez@csumb.edu>
2017-11-30 22:43:12 +0000

    update to simulator.cpp

--- simulator.cpp
+++ simulator.cpp
@@ -2,12 +2,15 @@
 #include <unordered_map>
 #include<fstream>
 #include <cstdlib>
+#include <cmath>
 #include <string>
 using namespace std;
 
 int bin_to_dec(string bin);
 int main() {
     ifstream file;
+    string inst;
+    int op;
     file.open("myFile.chkn");
 
     unordered_map<string, int> commands;
@@ -22,38 +25,39 @@ int main() {
     commands["1010"] = 0;
 
     while (!file.eof()) {
-        string opcode = stoi(file.substr(0, 3));
+        string opcode = inst.substr(0, 3);
+        op = stoi(opcode);
 
         if(opcode == "0000")//display -- ME
         {
-            string reg = stoi(file.substr(4,7));
+            string reg = inst.substr(4,7);
             cout << commands[reg] << endl;
         }
         else if (opcode =="0001")//input -- ME
         {
             int in =0;
             cin >> in;
-            commands[file.substr(4,7)] = in;
+            commands[inst.substr(4,7)] = in;
         }
         else if (opcode == "0010")//move
         {
-            string num1 = file.substr(4,7);
-            string num2 = file.substr(8,11);
+            string num1 = inst.substr(4,7);
+            string num2 = inst.substr(8,11);
             commands[num2]=commands[num1];
         }
         else if(opcode == "0011") // add
         {
-            string regs = file.substr(4,7);
-            string first = file.substr(8,11);
-            string second = file.substr(12,15);
+            string regs = inst.substr(4,7);
+            string first = inst.substr(8,11);
+            string second = inst.substr(12,15);
             commands[regs] = commands[first] + commands[second];
 
         }
         else if (opcode =="0100")//subtract -- ME
         {
-            string reg = file.substr(4,7);
-            string num1 = file.substr(8,11);
-            string num2 = fie.substr(12,15);
+            string reg = inst.substr(4,7);
+            string num1 = inst.substr(8,11);
+            string num2 = inst.substr(12,15);
 
             int sub = commands[num1] - commands [num2];
             commands[reg] = sub;
@@ -77,9 +81,9 @@ int main() {
         }
         else if (opcode == "0111") //module
         {
-            string reg = file.substr(4,7);
-            string num1 = file.substr(8,11);
-            string num2 = file.substr(12,15);
+            string reg = inst.substr(4,7);
+            string num1 = inst.substr(8,11);
+            string num2 = inst.substr(12,15);
             commands[reg] = commands[num1]%commands[num2];
         }
         else if (opcode == "1000")//jump
@@ -90,8 +94,8 @@ int main() {
         }
         else if (opcode == "1010")//put
         {
-            string reg = file.substr(4, 7);
-            string num = file.substr(8, 13);
+            string reg = inst.substr(4, 7);
+            string num = inst.substr(8, 13);
 
             int val = bin_to_dec(num);
             commands[reg] = val;
